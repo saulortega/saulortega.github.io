cac51a032e6a2dfb64e9d0b22f59e785da954fe7
Saúl Ortega <saulortega.co@gmail.com>
2020-03-07 23:21:11 -0500

    Agregando compilado en docs

---
+++ docs/precache-manifest.3458da94fecca48cfaefedeeaee3ae25.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6fb6af00b8965623061",
+    "url": "/css/app.b7e717c7.css"
+  },
+  {
+    "revision": "1fc7bfb93b7c92fb32d1",
+    "url": "/css/chunk-vendors.b56f2e8e.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "4ac24648104ce5b2da3765c8c1af19f5",
+    "url": "/img/yo.4ac24648.png"
+  },
+  {
+    "revision": "5153c1200f3af9b3faf83a90c6111661",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3bad28c33e3c1bc75cc2",
+    "url": "/js/about.a3d05327.js"
+  },
+  {
+    "revision": "c6fb6af00b8965623061",
+    "url": "/js/app.4f5289ca.js"
+  },
+  {
+    "revision": "1fc7bfb93b7c92fb32d1",
+    "url": "/js/chunk-vendors.358ababd.js"
+  },
+  {
+    "revision": "dcc7f1afa474016b835f8ea62f0cd88d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
