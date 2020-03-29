d29e09181c38c045af958f4a12eace2717a46d28
Saúl Ortega <saulortega.co@gmail.com>
2020-03-28 23:35:52 -0500

    Agregada funcionalidad en noscript

---
+++ precache-manifest.0fc8734ffd8b97dfa3551e5e7b809fc1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12bc05a6acf45e94aa5a",
+    "url": "/css/app.d55e1f9d.css"
+  },
+  {
+    "revision": "b07677c023bc0a8f2d68",
+    "url": "/css/chunk-vendors.b56f2e8e.css"
+  },
+  {
+    "revision": "ea3ae0059bf5549d7f29307c8aed3387",
+    "url": "/en/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "4ac24648104ce5b2da3765c8c1af19f5",
+    "url": "/img/yo.png"
+  },
+  {
+    "revision": "c484c10836e7e4351ac49fac3715e452",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3bad28c33e3c1bc75cc2",
+    "url": "/js/about.a3d05327.js"
+  },
+  {
+    "revision": "12bc05a6acf45e94aa5a",
+    "url": "/js/app.022fcb8d.js"
+  },
+  {
+    "revision": "b07677c023bc0a8f2d68",
+    "url": "/js/chunk-vendors.5d104cfe.js"
+  },
+  {
+    "revision": "dcc7f1afa474016b835f8ea62f0cd88d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
